2523c470261f12723cee7bd996494ce9b551388c
Zaziba Ahmed <ahmedzaziba@gmail.com>
2020-09-08 16:02:15 +0600

    Updates

---
+++ precache-manifest.1dd331110f51bea73865791563185d21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae365e124439dc390021518baa9ed270",
+    "url": "/Robo-Search/index.html"
+  },
+  {
+    "revision": "bfcb26e22b94ee8ab925",
+    "url": "/Robo-Search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b66d32bb9fbcd781e273",
+    "url": "/Robo-Search/static/css/main.af3ca6c1.chunk.css"
+  },
+  {
+    "revision": "bfcb26e22b94ee8ab925",
+    "url": "/Robo-Search/static/js/2.6caa75de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Search/static/js/2.6caa75de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66d32bb9fbcd781e273",
+    "url": "/Robo-Search/static/js/main.26703f80.chunk.js"
+  },
+  {
+    "revision": "7e3658a50a51576174d5",
+    "url": "/Robo-Search/static/js/runtime-main.36ebdff3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
